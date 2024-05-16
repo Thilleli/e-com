0e92837a005c003933222800b690e8ecba096633
Thilleli <lilybelhocine006@gmail.com>
2024-05-16 09:20:01 +0200

    manage image

---
+++ src/Entity/File.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FileRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FileRepository::class)]
+class File
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $fileName = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $type = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $public = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFileName(): ?string
+    {
+        return $this->fileName;
+    }
+
+    public function setFileName(?string $fileName): static
+    {
+        $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function isPublic(): ?bool
+    {
+        return $this->public;
+    }
+
+    public function setPublic(?bool $public): static
+    {
+        $this->public = $public;
+
+        return $this;
+    }
+}
---
+++ src/Service/imagesManager.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Service;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+class ImageManager
+{
+    protected $targetDirectory;
+    protected $subDirectory = "/images";
+
+    public function _construct($directory)
+    {
+        $this->targetDirectory = $directory;
+    }
+
+    public function getTargetDirectory(): string
+    {
+        return $this->targetDirectory;
+    }
+
+    public function getDirectory($public): string
+    {
+        if ($public === true) {
+            $directory = $this->targetDirectory.'/public';
+        } else {
+            $directory = $this->targetDirectory.'/private';
+        }
+
+        return $directory.$this->subDirectory;
+    }
+
+    public function upload(UploadedFile $file, bool $public = false): string
+    {
+        $fileName = null;
+        // $file_exists($this->targetDirectory."".$this->subDirectory.'/'.$fileName);
+//dd($this->getDirectory($public));
+        if (file_exists($this->getDirectory($public)) === false){
+            mkdir($this->getDirectory($public), 0777, true);
+        }
+
+        $count = 0;
+
+        while($count < 10 && $fileName == null || file_exists($this->getDirectory($public).'/'.$fileName)) {
+            $fileName = md5($file->getClientOriginalName()).
+                ''.
+                strreplace('.','',uniqid('',true)).
+                $file->guessExtension();
+            $count ++;
+        }
+
+        if ($count >= 10) {
+            throw new \Exception("Impossible de générer un nom de fichier unique aprés ".$count." tentatives");
+        }
+
+        $file->move($this->getDirectory($public), $fileName);
+
+        return $fileName;
+    }
+
+    public function stream(string $filePath): BinaryFileResponse
+    {
+        $response = new BinaryFileResponse($this->getDirectory(false)."/".$filePath);
+
+        return $response;
+    }
+}
